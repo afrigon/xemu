dfc36046f73d99c2cbedfdbcf4a918144ab8d79f
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2024-10-07 20:03:57 -0500

    Initial Commit

---
+++ xemu/xemuApp.swift
@@ -0,0 +1,17 @@
+//
+//  xemuApp.swift
+//  xemu
+//
+//  Created by Alexandre Frigon on 2024-10-07.
+//
+
+import SwiftUI
+
+@main
+struct xemuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xemuTests/xemuTests.swift
@@ -0,0 +1,16 @@
+//
+//  xemuTests.swift
+//  xemuTests
+//
+//  Created by Alexandre Frigon on 2024-10-07.
+//
+
+import Testing
+
+struct xemuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
